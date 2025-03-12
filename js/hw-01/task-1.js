f35f76817d27bda04afbc1a0eb20f24fd18aa384
Andriy Romanov <mgm.agro04@gmail.com>
2025-03-13 00:36:40 +0200

    hw-1 js

--- js/hw-01/task-1.js
+++ js/hw-01/task-1.js
@@ -1,12 +1,12 @@
-`use strict`;
-// Declare a makeTransaction function that expects two parameters,
-//  the values ​​of which will be set when it is called:
-// • quantity— the first parameter, a number containing the number of droids ordered •
-//  pricePerDroid — the second parameter, a number containing the cost of one droid
-//Add the function code so that it returns a string with a message about the purchase of repair droids:
-//  "You ordered <quantity> droids worth <totalPrice> credits!", where:
-//  • <quantity> is the number of droids ordered •
-// <totalPrice> is the total cost of the order, i.e. the cost of all ordered droids
+"use strict";
+/**Declare a makeTransaction function that expects two parameters,
+ * the values ​​of which will be set when it is called:
+ * quantity— the first parameter, a number containing the number of droids ordered
+ * pricePerDroid — the second parameter, a number containing the cost of one droid
+ * Add the function code so that it returns a string with a message about the purchase of repair droids:
+ * "You ordered <quantity> droids worth <totalPrice> credits!", where:
+ * <quantity> is the number of droids ordered
+ * <totalPrice> is the total cost of the order, i.e. the cost of all ordered droids */
 
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
